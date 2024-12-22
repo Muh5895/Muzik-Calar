b4aec9c15f3a99e67b4463766c68b02bcccdde35
Muh5895 <157362974+Muh5895@users.noreply.github.com>
2024-12-22 19:37:03 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = client => {
-
-  client.user.setActivity(`${client.config.prefix}help | ${client.config.prefix}open-source`)
-  console.log(`Bot ONLINE! (${client.user.tag} - ${client.user.id}) [Serving @ ${client.guilds.size} server]`);
-
-};
